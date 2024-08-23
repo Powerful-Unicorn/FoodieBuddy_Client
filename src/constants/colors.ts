b46c9a6bfe9f3fc454dbfd4a616103f7e661204e
doh-ha <101639449+doh-ha@users.noreply.github.com>
2024-08-23 18:37:33 +0900

    💄 로그인 input

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,7 +2,12 @@ const colors = {
   ORANGE_800: '#F27059',
   ORANGE_500: '#F79D65',
   ORANGE_200: '#F7B267',
-
+  RED_500: '#FF5F5F',
+  RED_300: '#FFB4B4B',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8E8E8E',
+  GRAY_700: '#575757',
   WHITE: '#FFF',
+  BLACK: '#000',
 };
 export {colors};
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,63 @@
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+import InputField from '../../components/InputField';
 
 function LoginScreen() {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+  const handleChangeText = (name: string, text: string) => {
+    setValues({
+      ...values,
+      [name]: text,
+    });
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="Email"
+          error={'Enter your email'}
+          touched={touched.email}
+          inputMode="email" //골뱅이 자판 추가
+          value={values.email}
+          onChangeText={text => handleChangeText('email', text)}
+          onBlur={() => handleBlur('email')}
+        />
+        <InputField
+          placeholder="Password"
+          error={'Enter your password'}
+          touched={touched.password}
+          secureTextEntry
+          value={values.password}
+          onChangeText={text => handleChangeText('password', text)}
+          onBlur={() => handleBlur('password')}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+});
 
 export default LoginScreen;
