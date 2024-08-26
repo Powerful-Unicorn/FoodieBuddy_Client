28e9b981361d4ccfb7b3ee01f01698c2551ef608
doh-ha <101639449+doh-ha@users.noreply.github.com>
2024-08-27 01:30:09 +0900

    💄 아이콘 설치

--- src/screens/chat/ChatScreen.tsx
+++ src/screens/chat/ChatScreen.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import {StyleSheet, View, Text, Pressable} from 'react-native';
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {MainDrawerParamList} from '../../navigations/drawer/MainDrawerNavigator';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {colors} from '../../constants';
 
 type Navigation = DrawerNavigationProp<MainDrawerParamList>;
 
@@ -11,6 +13,7 @@ function ChatScreen() {
   return (
     <View>
       <Text>채팅화면</Text>
+      <Ionicons name="albums" color={colors.ORANGE_500} size={100} />
     </View>
   );
 }
