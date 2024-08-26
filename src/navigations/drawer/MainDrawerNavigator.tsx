4a694a484460c008e2da5b492a20fffc6d1a67de
doh-ha <101639449+doh-ha@users.noreply.github.com>
2024-08-26 23:24:09 +0900

    💄 헤더 스타일 변경

--- src/navigations/drawer/MainDrawerNavigator.tsx
+++ src/navigations/drawer/MainDrawerNavigator.tsx
@@ -2,15 +2,33 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import ChatScreen from '../../screens/chat/ChatScreen';
 import SettingsScreen from '../../screens/settings/SettingsScreen';
 import BookmarksScreen from '../../screens/bookmarks/BookmarksScreen';
+import {colors, mainNavigations} from '../../constants';
 
-const Drawer = createDrawerNavigator();
+export type MainDrawerParamList = {
+  [mainNavigations.CHAT]: undefined;
+  [mainNavigations.BOOKMARKS]: undefined;
+  [mainNavigations.SETTINGS]: undefined;
+};
+const Drawer = createDrawerNavigator<MainDrawerParamList>();
 
 function MainDrawerNavigator() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Chat" component={ChatScreen} />
-      <Drawer.Screen name="Bookmarks" component={BookmarksScreen} />
-      <Drawer.Screen name="Settings" component={SettingsScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        drawerType: 'front',
+        headerTintColor: colors.ORANGE_800,
+        headerTitleStyle: {fontSize: 30},
+        headerStyle: {height: 120},
+      }}>
+      <Drawer.Screen name={mainNavigations.CHAT} component={ChatScreen} />
+      <Drawer.Screen
+        name={mainNavigations.BOOKMARKS}
+        component={BookmarksScreen}
+      />
+      <Drawer.Screen
+        name={mainNavigations.SETTINGS}
+        component={SettingsScreen}
+      />
     </Drawer.Navigator>
   );
 }
