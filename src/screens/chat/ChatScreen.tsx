e7e9ed81cb03077451290c5f4ff5d4dbc502ac06
doh-ha <101639449+doh-ha@users.noreply.github.com>
2024-08-26 23:39:47 +0900

    🎨 mainNavigation 생성중

--- src/screens/chat/ChatScreen.tsx
+++ src/screens/chat/ChatScreen.tsx
@@ -1,7 +1,13 @@
+import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '../../navigations/drawer/MainDrawerNavigator';
+
+type Navigation = DrawerNavigationProp<MainDrawerParamList>;
 
 function ChatScreen() {
+  const navigation = useNavigation<Navigation>();
   return (
     <View>
       <Text>채팅화면</Text>
