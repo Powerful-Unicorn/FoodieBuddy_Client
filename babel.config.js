933c749625d891c4f05cfa208a1c270cf4e9cc18
doh-ha <101639449+doh-ha@users.noreply.github.com>
2024-09-01 01:15:19 +0900

    ✨ chatGPT API 연결!!

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,23 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin', 'module:react-native-dotenv'],
+  //presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src', // src 경로에 대한 별칭 설정
+          //'@env': './env.d.ts', // @env 모듈에 대한 경로 설정
+        },
+      },
+    ],
+  ],
 };
